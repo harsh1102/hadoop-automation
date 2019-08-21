4f826636395b8317265fd31766ac17b6a53ece59
harsh1102 <harshmuniwala33@gmail.com>
2019-08-21 16:50:08 +0530

    update

--- pages/examples/register.php
+++ pages/examples/register.php
@@ -1,3 +1,61 @@
+<?php
+  require 'vendor/autoload.php';
+    
+  use Aws\DynamoDb\Exception\DynamoDbException;
+  use Aws\DynamoDb\DynamoDbClient;
+
+  // if((isset($_POST['submit'])) {
+
+      $client = DynamoDbClient::factory(array(
+          'profile' => 'default',
+          'region' => 'us-east-1',
+          'version' => '2018-08-21',
+          'credentials.cache' => true,
+          'validation' => false,
+          'scheme' => 'http'
+      ));
+
+      // $sdk = new Aws\Sdk([
+      //     'endpoint'   => 'http://3.87.118.50',
+      //     'region'   => 'us-east-1',
+      //     'version'  => 'latest'
+      // ]);
+      
+      // $dynamodb = $sdk->createDynamodb();
+      
+      $dataset = new Dataset();
+
+      $tablename = "Users";
+
+      $name = $_POST['fullname'];
+      $email = $_POST['email'];
+      $companyname = $_POST['companyname'];
+      $password = $_POST['pass'];
+
+      $item = $dataset->dataSetJson('
+      {
+          "name": '. $name .',
+          "email": '. $email .',
+          "companyname": '. $companyname .',
+          "password": '. $password .'
+      }
+      ');
+
+      $params = [
+          'TableName' => 'Users',
+          'Item' => $item
+      ];
+
+      try {
+          $result = $client->putItem($params);
+          echo "Added items";
+      
+      } catch (DynamoDbException $e) {
+          echo "Unable to add item:\n";
+          echo $e->getMessage() . "\n";
+      }
+  // }
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -96,7 +154,7 @@ input[type=submit] {
   <div class="register-box-body">
     <p class="login-box-msg">Register a new membership</p>
 
-    <form action="../../require/registration.php" method="post">
+    <form action="#" method="post">
       <div class="form-group has-feedback">
         <input type="text" class="form-control" placeholder="Full name" name="fullname" id="fullname" require>
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
