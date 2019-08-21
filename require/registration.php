28bcbcbedf5df1f8449b0d10f4f9d28c6d62cdfc
harsh1102 <harshmuniwala33@gmail.com>
2019-08-21 16:12:51 +0530

    test

--- require/registration.php
+++ require/registration.php
@@ -1,49 +1,50 @@
 <?php
-    require '../aws-autoloader.php';
-    date_default_timezone_set('UTC');
-    use Aws\DynamoDb\Exception\DynamoDbException;
+phpinfo();
+    // require 'aws-autoloader.php';
+    // date_default_timezone_set('UTC');
+    // use Aws\DynamoDb\Exception\DynamoDbException;
 
-    if((isset($_POST['submit'])) {
+    // if((isset($_POST['submit'])) {
 
-        $sdk = new Aws\Sdk([
-            'endpoint'   => 'http://3.87.118.50',
-            'region'   => 'us-east-1',
-            'version'  => 'latest'
-        ]);
+    //     $sdk = new Aws\Sdk([
+    //         'endpoint'   => 'http://3.87.118.50',
+    //         'region'   => 'us-east-1',
+    //         'version'  => 'latest'
+    //     ]);
         
-        $dynamodb = $sdk->createDynamodb();
+    //     $dynamodb = $sdk->createDynamodb();
         
-        $dataset = new Dataset();
+    //     $dataset = new Dataset();
 
-        $tablename = "Users";
+    //     $tablename = "Users";
 
-        $name = $_POST['fullname'];
-        $email = $_POST['email'];
-        $companyname = $_POST['companyname'];
-        $password = $_POST['pass'];
+    //     $name = $_POST['fullname'];
+    //     $email = $_POST['email'];
+    //     $companyname = $_POST['companyname'];
+    //     $password = $_POST['pass'];
 
-        $item = $dataset->dataSetJson('
-        {
-            "name": '. $name .',
-            "email": '. $email .',
-            "companyname": '. $companyname .',
-            "password": '. $password .'
-        }
-        ');
+    //     $item = $dataset->dataSetJson('
+    //     {
+    //         "name": '. $name .',
+    //         "email": '. $email .',
+    //         "companyname": '. $companyname .',
+    //         "password": '. $password .'
+    //     }
+    //     ');
 
-        $params = [
-            'TableName' => 'Users',
-            'Item' => $item
-        ];
+    //     $params = [
+    //         'TableName' => 'Users',
+    //         'Item' => $item
+    //     ];
 
-        try {
-            $result = $dynamodb->putItem($params);
-            echo "Added items";
+    //     try {
+    //         $result = $dynamodb->putItem($params);
+    //         echo "Added items";
         
-        } catch (DynamoDbException $e) {
-            echo "Unable to add item:\n";
-            echo $e->getMessage() . "\n";
-        }
-    }
+    //     } catch (DynamoDbException $e) {
+    //         echo "Unable to add item:\n";
+    //         echo $e->getMessage() . "\n";
+    //     }
+    // }
 
 ?>
