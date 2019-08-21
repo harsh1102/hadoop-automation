81f625d1bbfd1719231953de10ad45c7d02074d2
harsh1102 <harshmuniwala33@gmail.com>
2019-08-21 19:01:47 +0530

    test

--- require/registration.php
+++ require/registration.php
@@ -24,16 +24,12 @@
         $password = $_POST['pass'];
 
 
-        // $item = $marshaler->marshalJson('
-        //     {
-        //         "userid": 1,
-        //         "name": '. $name .',
-        //         "email": '. $email .',
-        //         "companyname": '. $companyname .',
-        //         "password": '. $password .'
-        //         "createdAt": knjmjinh
-        //     }
-        // ');
+        $item = $marshaler->marshalJson('
+            {
+                "userid": 1,
+                "createdAt": knjmjinh
+            }
+        ');
 
         // $params = [
         //     'TableName' => 'Users',
