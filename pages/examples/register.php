0f978cfe4ef90611e6c84e0e4ae34232573fad67
harsh1102 <harshmuniwala33@gmail.com>
2019-08-21 21:38:38 +0530

    register update

--- pages/examples/register.php
+++ pages/examples/register.php
@@ -14,31 +14,6 @@
 
   echo $userid;
   
-  if(isset($_POST['submit'])){
-
-    $name = $_POST['fullname'];
-    $email = $_POST['email'];
-    $companyname = $_POST['companyname'];
-    $password = $_POST['pass'];
-
-
-    $response = $client->putItem(array(
-        'TableName' => 'Users',
-        'Item' => array(
-            "userId" => array('S'      => $userid      ),
-            "name" => array('S'      => $name      ),
-            "email" => array('S'      => $email      ),
-            "companyname" => array('S'      => $companyname      ),
-            "password" => array('S'      => $password      ),
-            "createdAt" => array('S'      =>   date("d/m/Y")    )
-        )
-    ));
-
-    echo "<script type='text/javascript'>";
-    echo "alert('Successfully registerd');";
-    echo "window.location.href = 'login.php';";
-    echo "</script>";
-  }
 ?>
 
 
@@ -140,7 +115,7 @@ input[type=submit] {
   <div class="register-box-body">
     <p class="login-box-msg">Register a new membership</p>
 
-    <form action="" method="post">
+    <form action="../../reqiure/registration.php" method="post">
       <div class="form-group has-feedback">
         <input type="text" class="form-control" placeholder="Full name" name="fullname" id="fullname" require>
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
--- require/registration.php
+++ require/registration.php
@@ -1,20 +1,16 @@
 <?php
-    require 'vendor/autoload.php';
-
-    require_once('dbconfig.php');
-
     if(isset($_POST['submit'])){
 
         $name = $_POST['fullname'];
         $email = $_POST['email'];
         $companyname = $_POST['companyname'];
         $password = $_POST['pass'];
-
-
+    
+    
         $response = $client->putItem(array(
             'TableName' => 'Users',
             'Item' => array(
-                "userId" => array('S'      => '1'      ),
+                "userId" => array('S'      => $userid      ),
                 "name" => array('S'      => $name      ),
                 "email" => array('S'      => $email      ),
                 "companyname" => array('S'      => $companyname      ),
@@ -22,8 +18,11 @@
                 "createdAt" => array('S'      =>   date("d/m/Y")    )
             )
         ));
-
-        echo $response;
-    }
+    
+        echo "<script type='text/javascript'>";
+        echo "alert('Successfully registerd');";
+        echo "window.location.href = 'login.php';";
+        echo "</script>";
+      }
 
 ?>
