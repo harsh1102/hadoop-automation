c26af2df8f2e56f5dde87720fccedb71339d20d0
harsh1102 <harshmuniwala33@gmail.com>
2019-08-21 15:31:26 +0530

    URL update

--- pages/examples/register.php
+++ pages/examples/register.php
@@ -96,7 +96,7 @@ input[type=submit] {
   <div class="register-box-body">
     <p class="login-box-msg">Register a new membership</p>
 
-    <form action="../../require/register.php" method="post">
+    <form action="../../require/registration.php" method="post">
       <div class="form-group has-feedback">
         <input type="text" class="form-control" placeholder="Full name" name="fullname" id="fullname" require>
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
