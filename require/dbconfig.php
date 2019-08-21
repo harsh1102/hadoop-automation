8d7b71cec852101e399f2c6a356cd2c12319147e
harsh1102 <harshmuniwala33@gmail.com>
2019-08-21 20:39:48 +0530

    regitsrtion backend

---
+++ require/dbconfig.php
@@ -0,0 +1,16 @@
+<?php
+    use Aws\DynamoDb\Exception\DynamoDbException;
+    use Aws\DynamoDb\Marshaler;
+    use Aws\DynamoDb\DynamoDbClient;
+
+    date_default_timezone_set("Asia/Kolkata");
+
+    $client = DynamoDbClient::factory(array(
+        'profile' => 'default',
+        'region' => 'us-east-1',
+        'version' => '2012-08-10',
+        'credentials.cache' => true,
+        'validation' => false,
+        'scheme' => 'http'
+    ));
+?>
--- require/registration.php
+++ require/registration.php
@@ -1,76 +1,27 @@
 <?php
     require 'vendor/autoload.php';
     
-    use Aws\DynamoDb\Exception\DynamoDbException;
-    use Aws\DynamoDb\Marshaler;
-    use Aws\DynamoDb\DynamoDbClient;
+    require_once('dbconfig.php');
 
-    $client = DynamoDbClient::factory(array(
-        'profile' => 'default',
-        'region' => 'us-east-1',
-        'version' => '2012-08-10',
-        'credentials.cache' => true,
-        'validation' => false,
-        'scheme' => 'http'
-    ));
+    if(isset($_POST['submit'])){
 
-    $name = $_POST['fullname'];
-    $email = $_POST['email'];
-    $companyname = $_POST['companyname'];
-    $password = $_POST['pass'];
+        $name = $_POST['fullname'];
+        $email = $_POST['email'];
+        $companyname = $_POST['companyname'];
+        $password = $_POST['pass'];
 
 
-    $response = $client->putItem(array(
-        'TableName' => 'Users',
-        'Item' => array(
-            "userId" => array('S'      => '1'      ),
-            "name" => array('S'      => $name      ),
-            "email" => array('S'      => $email      ),
-            "companyname" => array('S'      => $companyname      ),
-            "password" => array('S'      => $password      ),
-            "createdAt" => array('S'      =>   date("d/m/Y")    )
-        )
-    ));
-
-    // date_default_timezone_set("Asia/Kolkata");
-
-    //     $sdk = new Aws\Sdk([
-    //         'endpoint'   => 'http://3.87.118.50',
-    //         'region'   => 'us-east-1',
-    //         'version'  => 'latest'
-    //     ]);
-        
-    //     $dynamodb = $sdk->createDynamoDb();
-    //     $marshaler = new Marshaler();
-
-
-    //     $tablename = "Users";
-
-    //     $name = $_POST['fullname'];
-    //     $email = $_POST['email'];
-    //     $companyname = $_POST['companyname'];
-    //     $password = $_POST['pass'];
-
-
-    //     $item = $marshaler->marshalJson('
-    //         {
-    //             "userid": 1,
-    //             "createdAt": knjmjinh
-    //         }
-    //     ');
-
-        // $params = [
-        //     'TableName' => 'Users',
-        //     'Item' => $item
-        // ];
-
-        // try {
-        //     $result = $dynamodb->putItem($params);
-        //     echo "Added items";
-        
-        // } catch (DynamoDbException $e) {
-        //     echo "Unable to add item:\n";
-        //     echo $e->getMessage() . "\n";
-        // }
+        $response = $client->putItem(array(
+            'TableName' => 'Users',
+            'Item' => array(
+                "userId" => array('S'      => '1'      ),
+                "name" => array('S'      => $name      ),
+                "email" => array('S'      => $email      ),
+                "companyname" => array('S'      => $companyname      ),
+                "password" => array('S'      => $password      ),
+                "createdAt" => array('S'      =>   date("d/m/Y")    )
+            )
+        ));
+    }
 
 ?>
