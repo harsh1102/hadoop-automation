3bc5f81e8f733ac3bc062474051bca32b8e6be6a
harsh1102 <harshmuniwala33@gmail.com>
2019-08-21 21:11:04 +0530

    register update

--- pages/examples/register.php
+++ pages/examples/register.php
@@ -2,30 +2,36 @@
 
   require_once('../../require/dbconfig.php');
 
+  $response = $client->scan(array(
+    'TableName' => 'Users'
+  ));
+
+  print_r (count($response['Item']));
+
   if(isset($_POST['submit'])){
 
-      $name = $_POST['fullname'];
-      $email = $_POST['email'];
-      $companyname = $_POST['companyname'];
-      $password = $_POST['pass'];
+    $name = $_POST['fullname'];
+    $email = $_POST['email'];
+    $companyname = $_POST['companyname'];
+    $password = $_POST['pass'];
 
 
-      $response = $client->putItem(array(
-          'TableName' => 'Users',
-          'Item' => array(
-              "userId" => array('S'      => '1'      ),
-              "name" => array('S'      => $name      ),
-              "email" => array('S'      => $email      ),
-              "companyname" => array('S'      => $companyname      ),
-              "password" => array('S'      => $password      ),
-              "createdAt" => array('S'      =>   date("d/m/Y")    )
-          )
-      ));
+    $response = $client->putItem(array(
+        'TableName' => 'Users',
+        'Item' => array(
+            "userId" => array('S'      => '1'      ),
+            "name" => array('S'      => $name      ),
+            "email" => array('S'      => $email      ),
+            "companyname" => array('S'      => $companyname      ),
+            "password" => array('S'      => $password      ),
+            "createdAt" => array('S'      =>   date("d/m/Y")    )
+        )
+    ));
 
-      echo "<script type='text/javascript'>";
-      echo "alert('Successfully registerd');";
-      echo "window.location.href = 'login.php';";
-      echo "</script>";
+    echo "<script type='text/javascript'>";
+    echo "alert('Successfully registerd');";
+    echo "window.location.href = 'login.php';";
+    echo "</script>";
   }
 ?>
 
