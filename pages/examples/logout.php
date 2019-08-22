119bebde3e100cd61ec761aa09f462c2dea79d40
harsh1102 <harshmuniwala33@gmail.com>
2019-08-22 15:50:41 +0530

    session  update

--- pages/examples/logout.php
+++ pages/examples/logout.php
@@ -1,8 +1,12 @@
 <?php
-    session_destroy();
 
+if(isset($_SESSION['user_email']))
+{
+    session_unset();
+    session_destroy();
     echo "<script type='text/javascript'>";
     echo "alert('Logout Successfully.');";
     echo "window.location.href = '/pages/examples/login.php';";
     echo "</script>";
+    }
 ?>
