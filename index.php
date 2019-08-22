2c4a1959463f678a94e4dffc4d1509065fc3dfe5
harsh1102 <harshmuniwala33@gmail.com>
2019-08-22 16:08:03 +0530

    index update

--- index.php
+++ index.php
@@ -86,25 +86,10 @@
                                             <li class="user-header">
                                                 <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
                                                 <p>
-                                                    <?php echo $_SESSION['user_email'] ?> - Web Developer
-                                                    <small>Member since Nov. 2012</small>
+                                                    <?php echo $_SESSION['user_email'] ?> - Cloud Developer
                                                 </p>
                                             </li>
                                             <!-- Menu Body -->
-                                            <li class="user-body">
-                                                <div class="row">
-                                                    <div class="col-xs-4 text-center">
-                                                        <a href="#">Followers</a>
-                                                    </div>
-                                                    <div class="col-xs-4 text-center">
-                                                        <a href="#">Sales</a>
-                                                    </div>
-                                                    <div class="col-xs-4 text-center">
-                                                        <a href="#">Friends</a>
-                                                    </div>
-                                                </div>
-                                                <!-- /.row -->
-                                            </li>
                                             <!-- Menu Footer-->
                                             <li class="user-footer">
                                                 <div class="pull-left">
